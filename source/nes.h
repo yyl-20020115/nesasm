e90162f879c283693d621cc1a2f8f2cbd115bf8a
Alexey 'Cluster' Avdyukhin <clusterrr@clusterrr.com>
2021-05-26 01:46:35 +0300

    Converted tabs to spaces

--- source/nes.h
+++ source/nes.h
@@ -17,51 +17,51 @@ void nes_inestim(int *ip);
 
 /* NES specific pseudos */
 struct t_opcode nes_pseudo[] = {
-	{NULL,  "DEFCHR",  nes_defchr,  PSEUDO, P_DEFCHR,  0},
-	{NULL,  "INESPRG", nes_inesprg, PSEUDO, P_INESPRG, 0},
-	{NULL,  "INESCHR", nes_ineschr, PSEUDO, P_INESCHR, 0},
-	{NULL,  "INESPRGRAM", nes_inesprgram, PSEUDO, P_INESPRGRAM, 0},
-	{NULL,  "INESCHRRAM", nes_ineschrram, PSEUDO, P_INESCHRRAM, 0},
-	{NULL,  "INESPRGNVRAM", nes_inesprgnvram, PSEUDO, P_INESPRGNVRAM, 0},
-	{NULL,  "INESCHRNVRAM", nes_ineschrnvram, PSEUDO, P_INESCHRNVRAM, 0},
-	{NULL,  "INESMAP", nes_inesmap, PSEUDO, P_INESMAP, 0},
-	{NULL,  "INESSUBMAP", nes_inessubmap, PSEUDO, P_INESSUBMAP, 0},
-	{NULL,  "INESMIR", nes_inesmir, PSEUDO, P_INESMIR, 0},
-	{NULL,  "INESBAT", nes_inesbat, PSEUDO, P_INESBAT, 0},
-	{NULL,  "INESTIM", nes_inestim, PSEUDO, P_INESTIM, 0},
+  {NULL,  "DEFCHR",  nes_defchr,  PSEUDO, P_DEFCHR,  0},
+  {NULL,  "INESPRG", nes_inesprg, PSEUDO, P_INESPRG, 0},
+  {NULL,  "INESCHR", nes_ineschr, PSEUDO, P_INESCHR, 0},
+  {NULL,  "INESPRGRAM", nes_inesprgram, PSEUDO, P_INESPRGRAM, 0},
+  {NULL,  "INESCHRRAM", nes_ineschrram, PSEUDO, P_INESCHRRAM, 0},
+  {NULL,  "INESPRGNVRAM", nes_inesprgnvram, PSEUDO, P_INESPRGNVRAM, 0},
+  {NULL,  "INESCHRNVRAM", nes_ineschrnvram, PSEUDO, P_INESCHRNVRAM, 0},
+  {NULL,  "INESMAP", nes_inesmap, PSEUDO, P_INESMAP, 0},
+  {NULL,  "INESSUBMAP", nes_inessubmap, PSEUDO, P_INESSUBMAP, 0},
+  {NULL,  "INESMIR", nes_inesmir, PSEUDO, P_INESMIR, 0},
+  {NULL,  "INESBAT", nes_inesbat, PSEUDO, P_INESBAT, 0},
+  {NULL,  "INESTIM", nes_inestim, PSEUDO, P_INESTIM, 0},
 
-	{NULL,  ".DEFCHR",  nes_defchr,  PSEUDO, P_DEFCHR,  0},
-	{NULL,  ".INESPRG", nes_inesprg, PSEUDO, P_INESPRG, 0},
-	{NULL,  ".INESCHR", nes_ineschr, PSEUDO, P_INESCHR, 0},
-	{NULL,  ".INESPRGRAM", nes_inesprgram, PSEUDO, P_INESPRGRAM, 0},
-	{NULL,  ".INESCHRRAM", nes_ineschrram, PSEUDO, P_INESCHRRAM, 0},
-	{NULL,  ".INESPRGNVRAM", nes_inesprgnvram, PSEUDO, P_INESPRGNVRAM, 0},
-	{NULL,  ".INESCHRNVRAM", nes_ineschrnvram, PSEUDO, P_INESCHRNVRAM, 0},
-	{NULL,  ".INESMAP", nes_inesmap, PSEUDO, P_INESMAP, 0},
-	{NULL,  ".INESSUBMAP", nes_inessubmap, PSEUDO, P_INESSUBMAP, 0},
-	{NULL,  ".INESMIR", nes_inesmir, PSEUDO, P_INESMIR, 0},
-	{NULL,  ".INESBAT", nes_inesbat, PSEUDO, P_INESBAT, 0},
-	{NULL,  ".INESTIM", nes_inestim, PSEUDO, P_INESTIM, 0},
+  {NULL,  ".DEFCHR",  nes_defchr,  PSEUDO, P_DEFCHR,  0},
+  {NULL,  ".INESPRG", nes_inesprg, PSEUDO, P_INESPRG, 0},
+  {NULL,  ".INESCHR", nes_ineschr, PSEUDO, P_INESCHR, 0},
+  {NULL,  ".INESPRGRAM", nes_inesprgram, PSEUDO, P_INESPRGRAM, 0},
+  {NULL,  ".INESCHRRAM", nes_ineschrram, PSEUDO, P_INESCHRRAM, 0},
+  {NULL,  ".INESPRGNVRAM", nes_inesprgnvram, PSEUDO, P_INESPRGNVRAM, 0},
+  {NULL,  ".INESCHRNVRAM", nes_ineschrnvram, PSEUDO, P_INESCHRNVRAM, 0},
+  {NULL,  ".INESMAP", nes_inesmap, PSEUDO, P_INESMAP, 0},
+  {NULL,  ".INESSUBMAP", nes_inessubmap, PSEUDO, P_INESSUBMAP, 0},
+  {NULL,  ".INESMIR", nes_inesmir, PSEUDO, P_INESMIR, 0},
+  {NULL,  ".INESBAT", nes_inesbat, PSEUDO, P_INESBAT, 0},
+  {NULL,  ".INESTIM", nes_inestim, PSEUDO, P_INESTIM, 0},
 
-	{NULL, NULL, NULL, 0, 0, 0}
+  {NULL, NULL, NULL, 0, 0, 0}
 };
 
 /* NES machine description */
 struct t_machine nes = {
-	MACHINE_NES,   /* type */
-	"NESASM", /* asm_name */
-	"NES Assembler (v3.0)", /* asm_title */
-	".nes",  /* rom_ext */
-	"NES_INCLUDE", /* include_env */
-	0x100,  /* zp_limit */
-	0x800,  /* ram_limit */
-	0,      /* ram_base */
-	0,      /* ram_page */
-	RESERVED_BANK, /* ram_bank */
-	NULL,       /* inst */
-	nes_pseudo, /* pseudo_inst */
-	nes_pack_8x8_tile, /* pack_8x8_tile */
-	NULL,              /* pack_16x16_tile */
-	NULL,              /* pack_16x16_sprite */
-	nes_write_header   /* write_header */
+  MACHINE_NES,   /* type */
+  "NESASM", /* asm_name */
+  "NES Assembler (v3.0)", /* asm_title */
+  ".nes",  /* rom_ext */
+  "NES_INCLUDE", /* include_env */
+  0x100,  /* zp_limit */
+  0x800,  /* ram_limit */
+  0,      /* ram_base */
+  0,      /* ram_page */
+  RESERVED_BANK, /* ram_bank */
+  NULL,       /* inst */
+  nes_pseudo, /* pseudo_inst */
+  nes_pack_8x8_tile, /* pack_8x8_tile */
+  NULL,              /* pack_16x16_tile */
+  NULL,              /* pack_16x16_sprite */
+  nes_write_header   /* write_header */
 };
--- source/symbol.c
+++ source/symbol.c
@@ -16,19 +16,19 @@
 int
 symhash(void)
 {
-	int i;
-	char c;
-	int hash = 0;
+  int i;
+  char c;
+  int hash = 0;
 
-	/* hash value */
-	for (i = 1; i <= symbol[0]; i++) {
-		c = symbol[i];
-		hash += c;
-		hash  = (hash << 3) + (hash >> 5) + c;
-	}
+  /* hash value */
+  for (i = 1; i <= symbol[0]; i++) {
+    c = symbol[i];
+    hash += c;
+    hash  = (hash << 3) + (hash >> 5) + c;
+  }
 
-	/* ok */
-	return (hash & 0xFF);
+  /* ok */
+  return (hash & 0xFF);
 }
 
 
@@ -43,108 +43,108 @@ symhash(void)
 int
 colsym(int *ip)
 {
-	int  err = 0;
-	int	 i = 0;
-	char c;
+  int  err = 0;
+  int   i = 0;
+  char c;
 
-	/* get the symbol */
-	for (;;) {
-		c = prlnbuf[*ip];
-		if (isdigit(c) && (i == 0))
-			break;
-		if ((!isalnum(c)) && (c != '_') && (c != '.'))
-			break;
-		if (i < (SBOLSZ - 1))
-			symbol[++i] = c;
-		(*ip)++;
-	}
+  /* get the symbol */
+  for (;;) {
+    c = prlnbuf[*ip];
+    if (isdigit(c) && (i == 0))
+      break;
+    if ((!isalnum(c)) && (c != '_') && (c != '.'))
+      break;
+    if (i < (SBOLSZ - 1))
+      symbol[++i] = c;
+    (*ip)++;
+  }
 
-	symbol[0] = i;
-	symbol[i+1] = '\0';
+  symbol[0] = i;
+  symbol[i+1] = '\0';
 
-	/* check if it's a reserved symbol */
-	if (i == 1) {
-		switch (toupper(symbol[1])) {
-		case 'A':
-		case 'X':
-		case 'Y':
-			err = 1;
-			break;
-		}
-	}
-	if (check_keyword())
-		err = 1;
+  /* check if it's a reserved symbol */
+  if (i == 1) {
+    switch (toupper(symbol[1])) {
+    case 'A':
+    case 'X':
+    case 'Y':
+      err = 1;
+      break;
+    }
+  }
+  if (check_keyword())
+    err = 1;
 
-	/* error */
-	if (err) {
-		fatal_error("Reserved symbol!");
-		return (0);
-	}	
+  /* error */
+  if (err) {
+    fatal_error("Reserved symbol!");
+    return (0);
+  }  
 
-	/* ok */
-	return (i);
+  /* ok */
+  return (i);
 }
 
 FILE* stlist_file(FILE *fp, char * basename, int n)
 {
-	if (fp) return fp;
-	char fname[256];
-	strcpy(fname, basename);
-	if (n < 0)
-		strcat(fname, ".ram.nl");
-	else {
-		char ext[16];
-		sprintf(ext, ".%X.nl", n);
-		strcat(fname, ext);
-	}
-	if ((fp = fopen(fname, "w")) == NULL) {
-		fprintf(stderr, "can not open file '%s'!\n", fname);
-		return 0;
-	}
-	return fp;
+  if (fp) return fp;
+  char fname[256];
+  strcpy(fname, basename);
+  if (n < 0)
+    strcat(fname, ".ram.nl");
+  else {
+    char ext[16];
+    sprintf(ext, ".%X.nl", n);
+    strcat(fname, ext);
+  }
+  if ((fp = fopen(fname, "w")) == NULL) {
+    fprintf(stderr, "can not open file '%s'!\n", fname);
+    return 0;
+  }
+  return fp;
 }
 
 void stlist(char *file, int bank_offset)
 {
-	struct t_symbol *sym;
-	struct t_symbol *local;
-	FILE *files[256];
-	memset(files, 0, sizeof(files)); 
-	int i, bank, fnum;
-	for (i = 0; i < sizeof(hash_tbl) / sizeof(hash_tbl[0]); i++)
-	{
-		sym = hash_tbl[i];
-		if (!sym) continue;
-		do
-		{
-			/* skipping reserved symbols and constants */
-			if (sym->reserved || sym->equ) continue;
+  struct t_symbol *sym;
+  struct t_symbol *local;
+  FILE *files[256];
+  memset(files, 0, sizeof(files)); 
+  int i, bank, fnum;
+  for (i = 0; i < sizeof(hash_tbl) / sizeof(hash_tbl[0]); i++)
+  {
+    sym = hash_tbl[i];
+    if (!sym) continue;
+    do
+    {
+      /* skipping reserved symbols and constants */
+      if (sym->reserved || sym->equ) continue;
 
-			bank = sym->value < 0x8000 ? -1 : sym->bank/2 + bank_offset;
-			fnum = bank >= 0 ? bank : (sizeof(files) / sizeof(FILE*) - 1);
-			files[fnum] = stlist_file(files[fnum], file, bank);
+      bank = sym->value < 0x8000 ? -1 : sym->bank/2 + bank_offset;
+      fnum = bank >= 0 ? bank : (sizeof(files) / sizeof(FILE*) - 1);
+      files[fnum] = stlist_file(files[fnum], file, bank);
       if (sym->data_size <= 1)
         fprintf(files[fnum], "$%04X#%s#\n", sym->value, sym->name+1);
       else
         fprintf(files[fnum], "$%04X/%02X#%s#\n", sym->value, sym->data_size, sym->name+1);
 
-			local = sym->local;
-			while (local)
-			{
-				bank = local->value < 0x8000 ? -1 : local->bank/2 + bank_offset;
-				fnum = bank >= 0 ? bank : (sizeof(files) / sizeof(FILE*) - 1);
-				files[fnum] = stlist_file(files[fnum], file, bank);
+      local = sym->local;
+      while (local)
+      {
+        bank = local->value < 0x8000 ? -1 : local->bank/2 + bank_offset;
+        fnum = bank >= 0 ? bank : (sizeof(files) / sizeof(FILE*) - 1);
+        files[fnum] = stlist_file(files[fnum], file, bank);
         if (sym->data_size <= 1)
           fprintf(files[fnum], "$%04X#%s (%s)#\n", local->value, local->name+1, sym->name+1);
         else
           fprintf(files[fnum], "$%04X/%02X#%s (%s)#\n", local->value, local->data_size, local->name+1, sym->name+1);
-   			local = local->next;
-			}
-		} while ((sym = sym->next) != NULL);
-	}
-	for (i = 0; i < sizeof(files) / sizeof(FILE*); i++)
-		if (files[i])
-			fclose(files[i]);
+         local = local->next;
+      }
+    } while ((sym = sym->next) != NULL);
+  }
+  for (i = 0; i < sizeof(files) / sizeof(FILE*); i++)
+    if (files[i])
+      fclose(files[i]);
 }
 
 /* ----
@@ -157,64 +157,64 @@ void stlist(char *file, int bank_offset)
 
 struct t_symbol *stlook(int flag)
 {
-	struct t_symbol *sym;
-	int sym_flag = 0;
-	int hash;
+  struct t_symbol *sym;
+  int sym_flag = 0;
+  int hash;
 
-	/* local symbol */
-	if (symbol[1] == '.') {
-		if (glablptr) {
-			/* search the symbol in the local list */
-			sym = glablptr->local;
+  /* local symbol */
+  if (symbol[1] == '.') {
+    if (glablptr) {
+      /* search the symbol in the local list */
+      sym = glablptr->local;
 
-			while (sym) {
-				if (!strcmp(symbol, sym->name))
-					break;			
-				sym = sym->next;
-			}
+      while (sym) {
+        if (!strcmp(symbol, sym->name))
+          break;      
+        sym = sym->next;
+      }
 
-			/* new symbol */
-			if (sym == NULL) {
-				if (flag) {
-					sym = stinstall(0, 1);
-					sym_flag = 1;
-				}
-			}
-		}
-		else {
-			error("Local symbol not allowed here!");
-			return (NULL);
-		}
-	}
+      /* new symbol */
+      if (sym == NULL) {
+        if (flag) {
+          sym = stinstall(0, 1);
+          sym_flag = 1;
+        }
+      }
+    }
+    else {
+      error("Local symbol not allowed here!");
+      return (NULL);
+    }
+  }
 
-	/* global symbol */
-	else {
-		/* search symbol */
-		hash = symhash();
-		sym = hash_tbl[hash];
-		while (sym) {
-			if (!strcmp(symbol, sym->name))
-				break;			
-			sym = sym->next;
-		}
+  /* global symbol */
+  else {
+    /* search symbol */
+    hash = symhash();
+    sym = hash_tbl[hash];
+    while (sym) {
+      if (!strcmp(symbol, sym->name))
+        break;      
+      sym = sym->next;
+    }
 
-		/* new symbol */
-		if (sym == NULL) {
-			if (flag) {
-				sym = stinstall(hash, 0);
-				sym_flag = 1;
-			}
-		}
-	}
+    /* new symbol */
+    if (sym == NULL) {
+      if (flag) {
+        sym = stinstall(hash, 0);
+        sym_flag = 1;
+      }
+    }
+  }
 
-	/* incremente symbol reference counter */
-	if (sym_flag == 0) {
-		if (sym)
-			sym->refcnt++;
-	}
+  /* incremente symbol reference counter */
+  if (sym_flag == 0) {
+    if (sym)
+      sym->refcnt++;
+  }
 
-	/* ok */
-	return (sym);
+  /* ok */
+  return (sym);
 }
 
 
@@ -226,46 +226,46 @@ struct t_symbol *stlook(int flag)
 
 struct t_symbol *stinstall(int hash, int type)
 {
-	struct t_symbol *sym;
+  struct t_symbol *sym;
 
-	/* allocate symbol structure */
-	if ((sym = (void *)malloc(sizeof(struct t_symbol))) == NULL) {
-		fatal_error("Out of memory!");
-		return (NULL);
-	}
+  /* allocate symbol structure */
+  if ((sym = (void *)malloc(sizeof(struct t_symbol))) == NULL) {
+    fatal_error("Out of memory!");
+    return (NULL);
+  }
 
-	/* init the symbol struct */
-	sym->type = if_expr ? IFUNDEF : UNDEF;
-	sym->value = 0;
-	sym->local = NULL;
-	sym->proc = NULL;
-	sym->bank = RESERVED_BANK;
-	sym->nb   = 0;
-	sym->size = 0;
-	sym->page = -1;
-	sym->vram = -1;
-	sym->pal   = -1;
-	sym->refcnt = 0;
-	sym->reserved = 0;
-	sym->equ = 0;
-	sym->data_type = -1;
-	sym->data_size = 0;
-	strcpy(sym->name, symbol);
+  /* init the symbol struct */
+  sym->type = if_expr ? IFUNDEF : UNDEF;
+  sym->value = 0;
+  sym->local = NULL;
+  sym->proc = NULL;
+  sym->bank = RESERVED_BANK;
+  sym->nb   = 0;
+  sym->size = 0;
+  sym->page = -1;
+  sym->vram = -1;
+  sym->pal   = -1;
+  sym->refcnt = 0;
+  sym->reserved = 0;
+  sym->equ = 0;
+  sym->data_type = -1;
+  sym->data_size = 0;
+  strcpy(sym->name, symbol);
 
-	/* add the symbol to the hash table */
-	if (type) {
-		/* local */
-		sym->next = glablptr->local;
-		glablptr->local = sym;
-	}
-	else {
-		/* global */
-		sym->next = hash_tbl[hash];
-		hash_tbl[hash] = sym;
-	}
+  /* add the symbol to the hash table */
+  if (type) {
+    /* local */
+    sym->next = glablptr->local;
+    glablptr->local = sym;
+  }
+  else {
+    /* global */
+    sym->next = hash_tbl[hash];
+    hash_tbl[hash] = sym;
+  }
 
-	/* ok */
-	return (sym);
+  /* ok */
+  return (sym);
 }
 
 
@@ -279,88 +279,88 @@ struct t_symbol *stinstall(int hash, int type)
 int
 labldef(int lval, int flag)
 {
-	char c;
+  char c;
 
-	/* check for NULL ptr */
-	if (lablptr == NULL)
-		return (0);
+  /* check for NULL ptr */
+  if (lablptr == NULL)
+    return (0);
 
-	/* adjust symbol address */	
-	if (flag)
-		lval = (lval & 0x1FFF) | (page << 13);
+  /* adjust symbol address */  
+  if (flag)
+    lval = (lval & 0x1FFF) | (page << 13);
 
-	/* first pass */
-	if (pass == FIRST_PASS) {
-		switch (lablptr->type) {
-		/* undefined */
-		case UNDEF:
-			lablptr->type = DEFABS;
-			lablptr->value = lval;
-			break;
+  /* first pass */
+  if (pass == FIRST_PASS) {
+    switch (lablptr->type) {
+    /* undefined */
+    case UNDEF:
+      lablptr->type = DEFABS;
+      lablptr->value = lval;
+      break;
 
-		/* already defined - error */
-		case IFUNDEF:
-			error("Can not define this label, declared as undefined in an IF expression!");
-			return (-1);
+    /* already defined - error */
+    case IFUNDEF:
+      error("Can not define this label, declared as undefined in an IF expression!");
+      return (-1);
 
-		case MACRO:
-			error("Symbol already used by a macro!");
-			return (-1);
+    case MACRO:
+      error("Symbol already used by a macro!");
+      return (-1);
 
-		case FUNC:
-			error("Symbol already used by a function!");
-			return (-1);
+    case FUNC:
+      error("Symbol already used by a function!");
+      return (-1);
 
-		default:
-			/* reserved label */
-			if (lablptr->reserved) {
-				fatal_error("Reserved symbol!");
-				return (-1);
-			}
+    default:
+      /* reserved label */
+      if (lablptr->reserved) {
+        fatal_error("Reserved symbol!");
+        return (-1);
+      }
 
-			/* compare the values */
-			if (lablptr->value == lval)
-				break;
+      /* compare the values */
+      if (lablptr->value == lval)
+        break;
 
-			/* normal label */
-			lablptr->type = MDEF;
-			lablptr->value = 0;
-			error("Label multiply defined!");
-			return (-1);
-		}
-	}
+      /* normal label */
+      lablptr->type = MDEF;
+      lablptr->value = 0;
+      error("Label multiply defined!");
+      return (-1);
+    }
+  }
 
-	/* second pass */
-	else {
-		if ((lablptr->value != lval) ||
-		   ((flag) && (bank < bank_limit) && (lablptr->bank != bank_base + bank)))
-		{
-			fatal_error("Internal error[1]!");
-			return (-1);
-		}
-	}
+  /* second pass */
+  else {
+    if ((lablptr->value != lval) ||
+       ((flag) && (bank < bank_limit) && (lablptr->bank != bank_base + bank)))
+    {
+      fatal_error("Internal error[1]!");
+      return (-1);
+    }
+  }
 
-	/* update symbol data */
-	if (flag) {
-		if (section == S_CODE)
-			lablptr->proc = proc_ptr;
-		lablptr->bank = bank_base + bank;
-		lablptr->page = page;
+  /* update symbol data */
+  if (flag) {
+    if (section == S_CODE)
+      lablptr->proc = proc_ptr;
+    lablptr->bank = bank_base + bank;
+    lablptr->page = page;
 
-		/* check if it's a local or global symbol */
-		c = lablptr->name[1];
-		if (c == '.')
-			/* local */
-			lastlabl = NULL;
-		else {
-			/* global */
-			glablptr = lablptr;
-			lastlabl = lablptr;
-		}
-	}
+    /* check if it's a local or global symbol */
+    c = lablptr->name[1];
+    if (c == '.')
+      /* local */
+      lastlabl = NULL;
+    else {
+      /* global */
+      glablptr = lablptr;
+      lastlabl = lablptr;
+    }
+  }
 
-	/* ok */
-	return (0);
+  /* ok */
+  return (0);
 }
 
 
@@ -373,25 +373,25 @@ labldef(int lval, int flag)
 void
 lablset(char *name, int val)
 {
-	int len;
+  int len;
 
-	len = strlen(name);
-	lablptr = NULL;
+  len = strlen(name);
+  lablptr = NULL;
 
-	if (len) {
-		symbol[0] = len;
-		strcpy(&symbol[1], name);
-		lablptr = stlook(1);
+  if (len) {
+    symbol[0] = len;
+    strcpy(&symbol[1], name);
+    lablptr = stlook(1);
 
-		if (lablptr) {
-			lablptr->type = DEFABS;
-			lablptr->value = val;
-			lablptr->reserved = 1;
-		}
-	}
+    if (lablptr) {
+      lablptr->type = DEFABS;
+      lablptr->value = val;
+      lablptr->reserved = 1;
+    }
+  }
 
-	/* ok */
-	return;
+  /* ok */
+  return;
 }
 
 
@@ -404,19 +404,19 @@ lablset(char *name, int val)
 void
 lablremap(void)
 {
-	struct t_symbol *sym;
-	int i;
+  struct t_symbol *sym;
+  int i;
 
-	/* browse the symbol table */
-	for (i = 0; i < 256; i++) {
-		sym = hash_tbl[i];
-		while (sym) {
-			/* remap the bank */
-			if (sym->bank <= bank_limit)
-				sym->bank += bank_base;
-			sym = sym->next;
-		}
-	}
+  /* browse the symbol table */
+  for (i = 0; i < 256; i++) {
+    sym = hash_tbl[i];
+    while (sym) {
+      /* remap the bank */
+      if (sym->bank <= bank_limit)
+        sym->bank += bank_base;
+      sym = sym->next;
+    }
+  }
 }
 
 
@@ -429,23 +429,23 @@ lablremap(void)
 void
 constset(char *name, int val)
 {
-	int len;
+  int len;
 
-	len = strlen(name);
-	lablptr = NULL;
+  len = strlen(name);
+  lablptr = NULL;
 
-	if (len) {
-		symbol[0] = len;
-		strcpy(&symbol[1], name);
-		lablptr = stlook(1);
+  if (len) {
+    symbol[0] = len;
+    strcpy(&symbol[1], name);
+    lablptr = stlook(1);
 
-		if (lablptr) {
-			lablptr->type = DEFABS;
-			lablptr->value = val;
-			lablptr->equ = 1;
-		}
-	}
+    if (lablptr) {
+      lablptr->type = DEFABS;
+      lablptr->value = val;
+      lablptr->equ = 1;
+    }
+  }
 
-	/* ok */
-	return;
+  /* ok */
+  return;
 }
