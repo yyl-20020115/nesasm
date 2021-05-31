9a96b65fc615ac211f046127b3a83ab63db0ebe7
Alexey 'Cluster' Avdyukhin <clusterrr@clusterrr.com>
2021-05-31 11:59:53 +0300

    extraneous parentheses fix

--- source/assemble.c
+++ source/assemble.c
@@ -199,7 +199,7 @@ assemble(void)
   flag = oplook(&ip);
   if (flag < 0) {
     labldef(loccnt, 1);
-    if ((flag == -1))
+    if (flag == -1)
       error("Unknown instruction!");
     if ((flag == -2) && (pass == LAST_PASS)) {
       if (lablptr)
